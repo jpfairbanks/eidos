155f130dda03555cb45af1a6d073a5bb74b68cd4
Ajay Nagesh <ajaynagesh@dhcp-10-134-206-147.uawifi.arizona.edu>
2018-03-21 12:08:38 -0700

    Running the entitiy finder on UN docs .. not using all the documetns .. also not parallelizing ...

---
+++ src/main/scala/org/clulab/wm/eidos/RunEntityFinder.scala
@@ -0,0 +1,50 @@
+package org.clulab.wm.eidos
+
+import java.io.{File, FileWriter}
+
+import org.clulab.processors.Processor
+import org.clulab.processors.fastnlp.FastNLPProcessor
+import org.clulab.serialization.json._
+
+/**
+  * Created by ajaynagesh on 3/19/18.
+  */
+object RunEntityFinder extends App {
+
+  val UNDocsDirPath = "../UNDocs/unrep_new_doc_jsons"
+  val UNDocsDir = new File(UNDocsDirPath)
+  val reader = new EidosSystem()
+  val filesList = UNDocsDir.listFiles().take(20)
+  val mentionsFile = new FileWriter(new File("../UNDocs/mentions.txt"))
+  val canonicalMentionsFile = new FileWriter(new File("../UNDocs/canonical_mentions.txt"))
+  val proc: Processor = new FastNLPProcessor()
+
+  val allCanonicalAndMentionsPar = for ((file,idx) <- filesList.zipWithIndex) yield { //.par.zipWithIndex) yield {
+
+    if(idx % 500 == 0)
+      println(s"Completed parsing ${idx} number of documents ....")
+
+    val doc = JSONSerializer.toDocument(file)
+    val text = doc.text.get
+    val annotatedDocument = reader.extractFromText(text)
+    val canonicalNames = annotatedDocument.eidosMentions.map(em => em.canonicalName)
+    val mentionTexts = annotatedDocument.odinMentions.map(om => om.text)
+    (canonicalNames, mentionTexts)
+  }
+  println(s"Finished parsing all the documents")
+
+  val (allCanonicalMentions, allMentions) = allCanonicalAndMentionsPar.toList.unzip
+  println(s"Dumping the mentions and the canonical mentions ...")
+
+  for (m <- allCanonicalMentions.flatten){
+    canonicalMentionsFile.write(s"${m}\n")
+  }
+  canonicalMentionsFile.close
+
+  for (m <- allMentions.flatten){
+    mentionsFile.write(s"${m}\n")
+  }
+  mentionsFile.close
+  println("DONE ...")
+
+}
