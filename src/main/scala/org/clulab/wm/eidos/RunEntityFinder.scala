430e8387a175abeb42534537899317945d682e3c
Ajay Nagesh <ajaynagesh@Ajays-MacBook-Pro.local>
2018-03-22 11:12:22 -0700

    explicit parallelization

--- src/main/scala/org/clulab/wm/eidos/RunEntityFinder.scala
+++ src/main/scala/org/clulab/wm/eidos/RunEntityFinder.scala
@@ -15,28 +15,64 @@ object RunEntityFinder extends App {
   val UNDocsDir = new File(UNDocsDirPath)
   val reader = new EidosSystem()
   val filesList = UNDocsDir.listFiles() //.take(1000) //.take(20)
-  val mentionsFile = new FileWriter(new File("../UNDocs/mentions.txt"))
-  val canonicalMentionsFile = new FileWriter(new File("../UNDocs/canonical_mentions.txt"))
   val proc: Processor = new FastNLPProcessor()
 
-  val allCanonicalAndMentionsPar = for (file <- filesList.par) yield { // filesList.zipWithIndex) yield { //
+  val numThreads = 40
 
-    val doc = JSONSerializer.toDocument(file)
-    val text = doc.text.get
-    val annotatedDocument = reader.extractFromText(text)
-    val canonicalNames = annotatedDocument.eidosMentions.map(em => em.canonicalName)
-    val mentionTexts = annotatedDocument.odinMentions.map(om => om.text)
+  //explicit parallelization
+  val filesListChunked = filesList.grouped(filesList.length/numThreads).toList
 
-    println(s"Completed parsing ${file.getName} ")
+  val mentionsFileList = (for (i <- (1 to numThreads)) yield new FileWriter(new File(s"../UNDocs/mentions_${i}.txt" ))).toList
+  val canonicalMentionsFileList = (for (i <- (1 to numThreads)) yield new FileWriter(new File(s"../UNDocs/canonical_mentions_${i}.txt" ))).toList
 
-    mentionsFile.write(s"${mentionTexts.mkString("\n")}\n")
-    canonicalMentionsFile.write(s"${canonicalNames.mkString("\n")}\n")
-    mentionsFile.flush
-    canonicalMentionsFile.flush
+  for (i <- (1 to numThreads).par) {
+    for (file <- filesListChunked(i)) {
+      val doc = JSONSerializer.toDocument(file)
+      val text = doc.text.get
+      val annotatedDocument = reader.extractFromText(text)
+      val canonicalNames = annotatedDocument.eidosMentions.map(em => em.canonicalName)
+      val mentionTexts = annotatedDocument.odinMentions.map(om => om.text)
 
-    (canonicalNames, mentionTexts)
+      println(s"Completed parsing ${file.getName} ")
+
+      mentionsFileList(i).write(s"${mentionTexts.mkString("\n")}\n")
+      canonicalMentionsFileList(i).write(s"${canonicalNames.mkString("\n")}\n")
+      mentionsFileList(i).flush
+      canonicalMentionsFileList(i).flush
+
+    }
   }
+
   println(s"Finished parsing all the documents")
+
+  for (i <- 1 to numThreads){
+    mentionsFileList(i).close
+    canonicalMentionsFileList(i).close
+  }
+
+//  val mentionsFile = new FileWriter(new File("../UNDocs/mentions.txt"))
+//  val canonicalMentionsFile = new FileWriter(new File("../UNDocs/canonical_mentions.txt"))
+//  val allCanonicalAndMentionsPar = for (file <- filesList.par) yield { // filesList.zipWithIndex) yield { //
+//
+//    val doc = JSONSerializer.toDocument(file)
+//    val text = doc.text.get
+//    val annotatedDocument = reader.extractFromText(text)
+//    val canonicalNames = annotatedDocument.eidosMentions.map(em => em.canonicalName)
+//    val mentionTexts = annotatedDocument.odinMentions.map(om => om.text)
+//
+//    println(s"Completed parsing ${file.getName} ")
+//
+//    mentionsFile.write(s"${mentionTexts.mkString("\n")}\n")
+//    canonicalMentionsFile.write(s"${canonicalNames.mkString("\n")}\n")
+//    mentionsFile.flush
+//    canonicalMentionsFile.flush
+//
+//    (canonicalNames, mentionTexts)
+//  }
+//  println(s"Finished parsing all the documents")
+//
+//  mentionsFile.close
+//  canonicalMentionsFile.close
 
 //  val (allCanonicalMentions, allMentions) = allCanonicalAndMentionsPar.toList.unzip
 //  println(s"Dumping the mentions and the canonical mentions ...")
@@ -44,12 +80,11 @@ object RunEntityFinder extends App {
 //  for (m <- allCanonicalMentions.flatten){
 //    canonicalMentionsFile.write(s"${m}\n")
 //  }
-  canonicalMentionsFile.close
 //
 //  for (m <- allMentions.flatten){
 //    mentionsFile.write(s"${m}\n")
 //  }
-  mentionsFile.close
+
 //  println("DONE ...")
 
 }
